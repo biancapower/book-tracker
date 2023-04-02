67448381ed0fda3b5bd6fedf0ebf4f8fb0a9429d
Bianca Power <bianca@biancapower.com>
2023-04-02 14:24:53 +1000

    Implement as PWA
    It is now downloadable as a PWA on mobile devices

---
+++ public/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'your-app-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  // FIXME: make sure css and js assets are being cached
+  // '/assets/stylesheets/application.css',
+  // '/javascript/application.js',
+  // '/favicon.ico',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          let responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
